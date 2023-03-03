abab04c055d7f702406fb1b4f4828425c5d04739
Amirreza <amirreza.tm86@gmail.com>
2023-03-03 10:32:37 -0500

    todoapp

--- front-end/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import store from '../store/store'
-import App from "./App.vue";
-import router from '../routes/router'
-import './axios'
-
-const app = createApp(App).use(store).use(router)
-app.mount("#app");
